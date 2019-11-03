dd2470c198f56ac54717eac1227976bb90161717
Csinga24 <oszlanyi.csenge624@gmail.com>
2019-11-03 15:52:13 +0100

    add functions to service classes

--- src/main/java/webshop/service/ProductService.java
+++ src/main/java/webshop/service/ProductService.java
@@ -1,8 +1,31 @@
 package webshop.service;
 
+import webshop.model.Product;
+import webshop.model.ProductPrice;
+
+
 import webshop.repository.ProductRepository;
 
 public class ProductService {
+
     ProductRepository productRepository;
 
+    void addProduct(String name, int number, String description, ProductPrice price) {
+        if (productRepository.existsbyName(name)) {
+            Product p = Product.createNewProduct(name, number, description, price);
+
+            productRepository.save(p);
+        }
+    }
+
+    void deleteProduct(String name) {
+        Product p = productRepository.findbyName(name);
+        productRepository.delete(p);
+    }
+
+    void changeNumberInStock(int number, String name) { //Add new items (+) or buy some (-)
+        Product p = productRepository.findbyName(name);
+        p.setNumberInStock(p.getNumberInStock() + number);
+        productRepository.save(p);
+    }
 }
--- src/main/java/webshop/service/ShoppingCartService.java
+++ src/main/java/webshop/service/ShoppingCartService.java
@@ -4,8 +4,26 @@ import webshop.repository.CustomerRepository;
 import webshop.repository.ProductRepository;
 import webshop.repository.ShoppingCartRepository;
 
+import webshop.model.Product;
+import webshop.model.Customer;
+import webshop.model.ShoppingCart;
+
 public class ShoppingCartService {
+
     ProductRepository productRepository;
     ShoppingCartRepository shoppingCartRepository;
     CustomerRepository customerRepository;
+
+
+    void addProductToCart(String productName, int customerId) {
+        Product p = productRepository.findbyName(productName);
+        Customer u = customerRepository.findbyId(customerId);
+        ShoppingCart sc = u.getShoppingCart();
+
+        sc.addProduct(p);
+
+        shoppingCartRepository.save(sc);
+        customerRepository.save(u);
+    }
+
 }
