5e1bca504478630865f5bbf463a41b5725b6bfce
angyaloliver <oliver.angyal@gmail.com>
2019-11-13 17:45:13 +0100

    Add ProductRepository

--- src/main/java/webshop/repository/ProductRepository.java
+++ src/main/java/webshop/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package webshop.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import webshop.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+  List<Product> findByName(String name);
 }
