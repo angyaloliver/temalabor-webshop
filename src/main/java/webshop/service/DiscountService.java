398e791b077378c99356ae9e55f5c41bb1fc14d9
angyaloliver <oliver.angyal@gmail.com>
2019-11-13 18:45:03 +0100

    Add discountProductsInCategory to DiscountService.

--- src/main/java/webshop/service/DiscountService.java
+++ src/main/java/webshop/service/DiscountService.java
@@ -1,5 +1,36 @@
 package webshop.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import webshop.model.Product;
+import webshop.model.ProductCategory;
+import webshop.repository.ProductCategoryRepository;
+import webshop.repository.ProductRepository;
+
+@Service
 public class DiscountService {
+
+  @Autowired
+  ProductRepository productRepository;
+
+  @Autowired
+  ProductCategoryRepository productCategoryRepository;
+
+  @Transactional
+  public void discountProductsInCategory(String categoryName, int percent) {
+
+    List<ProductCategory> categories = productCategoryRepository.findByName(categoryName);
+
+    for (ProductCategory productCategory : categories) {
+      for (Product product : productCategory.getProducts()) {
+        product.discount();
+      }
+    }
+  }
+
 
 }
