f95d45ef4d245e046b939847175040a5b1453971
angyaloliver <oliver.angyal@gmail.com>
2019-11-14 02:48:12 +0100

    Add DiscountService tests.

---
+++ src/test/java/webshop/service/DiscountServiceIT.java
@@ -0,0 +1,63 @@
+package webshop.service;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import webshop.model.Price;
+import webshop.model.ProductCategory;
+import webshop.model.Product;
+import webshop.repository.ProductCategoryRepository;
+import webshop.repository.ProductRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase
+public class DiscountServiceIT {
+
+  @Autowired
+  DiscountService discountService;
+
+  @Autowired
+  ProductCategoryRepository productCategoryRepository;
+
+  @Autowired
+  ProductRepository productRepository;
+
+  @Test
+  public void testDiscountProductsInCategory() throws Exception {
+
+    Product product1 = new Product(1, "Product 1",
+        new Price(BigDecimal.valueOf(100.0), BigDecimal.valueOf(1.27)));
+    Product product2 = new Product(2, "Product2",
+        new Price(BigDecimal.valueOf(200.0), BigDecimal.valueOf(1.19)));
+    product1 = productRepository.save(product1);
+    product2 = productRepository.save(product2);
+
+    ProductCategory productCategory = new ProductCategory("ABC");
+    productCategory.addProduct(product1);
+    productCategory.addProduct(product2);
+    productCategoryRepository.save(productCategory);
+
+    BigDecimal delta = BigDecimal.valueOf(0.0001);
+    assertThat(product1.getOriginalPrice().getGross(), closeTo(BigDecimal.valueOf(127.0), delta));
+    assertThat(product2.getOriginalPrice().getNet(), closeTo(BigDecimal.valueOf(200.0), delta));
+
+    discountService.discountProductsInCategory("ABC", BigDecimal.valueOf(0.8));
+
+    assertThat(product1.getOriginalPrice().getGross(), closeTo(BigDecimal.valueOf(101.6), delta));
+    assertThat(product2.getOriginalPrice().getNet(), closeTo(BigDecimal.valueOf(160.0), delta));
+  }
+
+}
---
+++ src/test/java/webshop/service/DiscountServiceTest.java
@@ -0,0 +1,54 @@
+package webshop.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.closeTo;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import webshop.model.Price;
+import webshop.model.Product;
+import webshop.model.ProductCategory;
+import webshop.repository.ProductCategoryRepository;
+import webshop.repository.ProductRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DiscountServiceTest {
+
+  @InjectMocks
+  DiscountService discountService;
+
+  @Mock
+  ProductCategoryRepository categoryRepository;
+
+  @Mock
+  ProductRepository productRepository;
+
+  @Test
+  public void testDiscountProductsInCategory() throws Exception {
+
+    ProductCategory productCategory = new ProductCategory("ABC");
+    Product product1 = new Product(1, "Product 1",
+        new Price(BigDecimal.valueOf(100.0), BigDecimal.valueOf(1.27)));
+    Product product2 = new Product(2, "Product2",
+        new Price(BigDecimal.valueOf(200.0), BigDecimal.valueOf(1.19)));
+    productCategory.addProduct(product1);
+    productCategory.addProduct(product2);
+
+    when(categoryRepository.findByName("ABC")).thenReturn(Arrays.asList(productCategory));
+
+    BigDecimal delta = BigDecimal.valueOf(0.0001);
+    assertThat(product1.getOriginalPrice().getGross(), closeTo(BigDecimal.valueOf(127.0), delta));
+    assertThat(product2.getOriginalPrice().getNet(), closeTo(BigDecimal.valueOf(200.0), delta));
+
+    discountService.discountProductsInCategory("ABC", BigDecimal.valueOf(0.8));
+
+    assertThat(product1.getOriginalPrice().getGross(), closeTo(BigDecimal.valueOf(101.6), delta));
+    assertThat(product2.getOriginalPrice().getNet(), closeTo(BigDecimal.valueOf(160.0), delta));
+  }
+}
