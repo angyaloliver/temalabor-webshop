ba3c6563de9ee8a32a87793e659ad242cf82c7fc
angyaloliver <oliver.angyal@gmail.com>
2019-11-18 12:43:44 +0100

    Modify modifyProduct test.

--- src/test/java/webshop/service/ProductServiceTest.java
+++ src/test/java/webshop/service/ProductServiceTest.java
@@ -25,6 +25,48 @@ public class ProductServiceTest {
   ProductRepository productRepository;
 
   @Test
+  public void testModifyProduct() {
+    Integer productToBeModifiedId = 9999;
+
+    Product productToBeModified = Product.builder()
+        .id(productToBeModifiedId)
+        .name("ProductToBeModified")
+        .description("This product has to be modified.")
+        .originalPrice(new Price(new BigDecimal("2500"), new BigDecimal("0.27")))
+        .build();
+
+    Integer modifyingProductId = 1111;
+    String modifiedName = "ModifiedProduct";
+    String modifiedDescription = "This product has been modified.";
+    int numberInStock = 5;
+
+    Product modifyingProduct = Product.builder()
+        .id(modifyingProductId)
+        .name(modifiedName)
+        .description(modifiedDescription)
+        .productCategories(new HashSet<>())
+        .images(new HashSet<>())
+        .numberInStock(numberInStock)
+        .build();
+
+    Mockito.when(productRepository.getOne(productToBeModifiedId)).thenReturn(productToBeModified);
+
+    productService.modifyProduct(productToBeModifiedId, modifyingProduct);
+
+    Assert.assertEquals("ModifiedProduct",
+        productService.getProductById(productToBeModifiedId).getName());
+    Assert.assertEquals("This product has been modified.",
+        productService.getProductById(9999).getDescription());
+    Assert.assertEquals(5, productService.getProductById(productToBeModifiedId).getNumberInStock());
+    Assert.assertNull(productService.getProductById(productToBeModifiedId).getOriginalPrice());
+    Assert.assertEquals(Collections.EMPTY_SET,
+        productService.getProductById(productToBeModifiedId).getProductCategories());
+    Assert.assertEquals(Collections.EMPTY_SET,
+        productService.getProductById(productToBeModifiedId).getImages());
+    Assert.assertNull(productService.getProductById(productToBeModifiedId).getDiscountRate());
+  }
+
+  @Test
   public void testChangeNumberInStock() {
     Integer productId = 9999;
     int initialQuantity = 50;
@@ -43,37 +85,5 @@ public class ProductServiceTest {
     productService.changeNumberInStock(productId, newQuantity);
 
     Assert.assertEquals(newQuantity, productService.getProductById(productId).getNumberInStock());
-  }
-
-  @Test
-  public void testModifyProduct() {
-    Integer productToBeModifiedId = 9999;
-    Integer modifyingProductId = 1111;
-
-    Product productToBeModified = Product.builder()
-        .id(productToBeModifiedId)
-        .name("ProductToBeModified")
-        .description("This product has to be modified.")
-        .originalPrice(new Price(new BigDecimal("2500"), new BigDecimal("0.27")))
-        .build();
-
-    Product modifyingProduct = Product.builder()
-        .id(modifyingProductId)
-        .name("ModifiedProduct")
-        .description("This product has been modified.")
-        .productCategories(new HashSet<>())
-        .numberInStock(5)
-        .build();
-
-    Mockito.when(productRepository.getOne(productToBeModifiedId)).thenReturn(productToBeModified);
-
-    productService.modifyProduct(productToBeModifiedId, modifyingProduct);
-
-    Assert.assertEquals("ModifiedProduct", productService.getProductById(9999).getName());
-    Assert.assertEquals("This product has been modified.",
-        productService.getProductById(9999).getDescription());
-    Assert.assertEquals(5, productService.getProductById(9999).getNumberInStock());
-    Assert.assertNull(productService.getProductById(9999).getOriginalPrice());
-    Assert.assertEquals(Collections.EMPTY_SET, productService.getProductById(9999).getProductCategories());
   }
 }
